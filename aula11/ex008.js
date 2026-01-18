554a03640b802ba6d418610c69d0aa7075bdb0a0
Rafael Liger <rafael-liger@outlook.com>
2026-01-18 20:08:43 -0300

    if else

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){
+    console.log('você está acima da velocidade permitida.')
+
+}
+console.log('Dirija sempre usando cinto de segurança!')
