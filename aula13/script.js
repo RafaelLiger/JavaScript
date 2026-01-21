dbb2c3e699e888b8d256a3436caedfa6a33068cb
Rafael Liger <rafael-liger@outlook.com>
2026-01-21 12:11:36 -0300

    ex14 pronto

--- aula13/script.js
+++ aula13/script.js
@@ -1,1 +1,22 @@
-alert ('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora < 12) {
+        //Bom dia!
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#cddce3'
+    }else if (hora >= 12 && hora < 18) {
+        //Boa tarde!
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#d86c01'
+    }else {
+        //Boa noite!
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#09181b'
+    }
+
+}
