8b96217415bc8bcc54f505166e535b02c441141d
Rafael Liger <rafael-liger@outlook.com>
2026-01-23 19:34:49 -0300

    while do

---
+++ aula13-2/while.js
@@ -0,0 +1,11 @@
+let A = 1
+let B = 1
+//Aperte F8 para ver como funciona
+while (A <= 6) {
+    console.log(`tick ${A}`)
+    A++
+}
+do {
+    console.log(`tack ${B}`)
+    B++    
+}while(B <= 6)
