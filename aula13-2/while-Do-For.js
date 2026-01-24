3efa52d20aade9c0d096dc86a34f3f58b9acfca0
Rafael Liger <rafael-liger@outlook.com>
2026-01-24 14:47:08 -0300

    ex prontos

---
+++ aula13-2/while-Do-For.js
@@ -0,0 +1,21 @@
+let A = 1
+let B = 1
+//let C = 1 esta dentro do for
+//Aperte F8 para ver como funciona
+
+// Estrutura de repetição While
+while (A <= 6) {
+    console.log(`tick ${A}`)
+    A++
+}
+
+// Estrutura de repetição Do While
+do {
+    console.log(`tack ${B}`)
+    B++    
+}while(B <= 6)
+
+// Estrutura de repetição For
+for(let C = 1; C <= 6;C++) {
+    console.log(`tock ${C}`)
+}
