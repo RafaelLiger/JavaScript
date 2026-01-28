3bcb61aa918e896183981b31aa65e5bbe3c82286
Rafael Liger <rafael-liger@outlook.com>
2026-01-28 15:51:18 -0300

    inicio

---
+++ exfinal/script.js
@@ -0,0 +1,7 @@
+let num = document.querySelector('#txtnum')
+let add = document.querySelector('#adicionar')
+let caixa = document.querySelector('#caixanum')
+let final = document.querySelector('#finalizar')
+add.addEventListener('click', clicadd)
+final.addEventListener('click', clicfin)
+
