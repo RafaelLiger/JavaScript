f8e5b12e12b345b4871d6d949f0f24dc1a7a8221
Rafael Liger <rafael-liger@outlook.com>
2026-01-30 09:39:37 -0300

    ,,

--- desafiogpt/d2.js
+++ desafiogpt/d2.js
@@ -1,4 +1,12 @@
-for(let c = 10; c >= 1; c-- ) {
-    console.log(c)
+let vet = [1, 2 ,3 , 5, 8]
+console.log(vet)
+console.log(vet.length)
 
+let maior =  vet[0]
+
+for (let pos =0; pos < vet.length; pos++){
+    if ( pos > maior){
+        maior = pos
+    }    
 }
+console.log
