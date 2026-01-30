ffed00ba2c4d4ad5d453f32d05df0c371c26d5fe
Rafael Liger <rafael-liger@outlook.com>
2026-01-30 09:18:48 -0300

    ..

---
+++ desafiogpt/d2.js
@@ -0,0 +1,4 @@
+for(let c = 10; c >= 1; c-- ) {
+    console.log(c)
+
+}
--- desafiogpt/script.js
+++ desafiogpt/script.js
@@ -1,10 +1,14 @@
 let idade = document.querySelector('#idade')
 let nome = document.querySelector('#nome')
-
+let res = document.querySelector('#res')
 function confirmar() {
     
     if (idade.value.length == 0 || nome.value.length == 0) {
         alert('Digite seu nome e sua idade!')
 
+    }else{
+        let n = nome.value
+        let id = Number(idade.value)
+        res.innerHTML = `Olá ${n} você tem ${id} anos de idade`
     }
 }
