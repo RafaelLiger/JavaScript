0bf1b66f021cba6f455595a4709be395b267e35a
Rafael Liger <rafael-liger@outlook.com>
2026-01-30 23:00:12 -0300

    ..

--- aula14-vetores/ambiente.js
+++ aula14-vetores/ambiente.js
@@ -1,6 +1,14 @@
-let num =[5, 8, 2, 9, 3]
-num.sort()
+let num = [5,8,2,9,3,40]
 num.push(1)
+/*Se o resultado da função for negativo (), significa que  deve vir antes de .
+• 	Se o resultado for positivo (), significa que  deve vir depois de .
+• 	Se o resultado for zero (), significa que são considerados iguais e não muda a ordem.
+
+num.sort(function(a, b){
+    return a - b 
+}) */
+//abaixo a mesma função em arrow function
+num.sort((a, b) => a - b)
 console.log(num)
 console.log(`O valor tem ${num.length} posições`)
 console.log (`O primeiro valor do vetor é ${num[0]}`)
--- desafiogpt/d2.js
+++ desafiogpt/d2.js
@@ -1,12 +1,25 @@
-let vet = [1, 2 ,3 , 5, 8]
-console.log(vet)
+let vet = [3, 2 , 5 , 7, 8, 4]
 console.log(vet.length)
+vet.sort((a, b) => a - b)
+console.log(vet)
+let soma = 0
 
 let maior =  vet[0]
+let menor = vet[0]
 
-for (let pos =0; pos < vet.length; pos++){
-    if ( pos > maior){
-        maior = pos
-    }    
+for (let pos in vet){
+    soma += vet[pos]
+    if ( vet[pos] > maior){
+        maior = vet[pos]   
+    }
+    if( vet[pos] < menor ) {
+        menor = vet[pos]    
+    }
 }
-console.log
+let media = soma / vet.length
+
+console.log(`O valor maior foi ${maior}`)
+console.log(`O valor menor foi ${menor}`)
+console.log(`O valor da soma foi ${soma}`)
+console.log(`O valor da media foi ${media.toFixed(2)}`)
+
