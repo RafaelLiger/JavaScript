8a5e7aeb099436b826225138f0b8a96f5cf64bd5
Rafael Liger <rafael-liger@outlook.com>
2026-01-31 12:50:53 -0300

    ..

--- desafiogpt/d2.js
+++ desafiogpt/d2.js
@@ -1,25 +1,31 @@
-let vet = [3, 2 , 5 , 7, 8, 4]
-console.log(vet.length)
-vet.sort((a, b) => a - b)
-console.log(vet)
-let soma = 0
 
-let maior =  vet[0]
-let menor = vet[0]
+function analisar(vet) {
+    let soma = 0
+    let maior = vet[0]
+    let menor = vet[0]
 
-for (let pos in vet){
-    soma += vet[pos]
-    if ( vet[pos] > maior){
-        maior = vet[pos]   
+    for (let pos in vet) {
+        soma += vet[pos]
+
+        if (vet[pos] > maior) {
+            maior = vet[pos]
+        }
+
+        if (vet[pos] < menor) {
+            menor = vet[pos]
+        }
     }
-    if( vet[pos] < menor ) {
-        menor = vet[pos]    
+
+    let media = soma / vet.length
+
+    return {
+        maior: maior,
+        menor: menor,
+        soma: soma,
+        media: media.toFixed(2)
     }
 }
-let media = soma / vet.length
+let numeros = [3, 2, 5, 7, 8, 4]
+let resultado = analisar(numeros)
 
-console.log(`O valor maior foi ${maior}`)
-console.log(`O valor menor foi ${menor}`)
-console.log(`O valor da soma foi ${soma}`)
-console.log(`O valor da media foi ${media.toFixed(2)}`)
-
+console.log(resultado)
