c9838657485d6a7ec6d9b672026abfd5d70c568c
Rafael Liger <rafael-liger@outlook.com>
2026-03-24 18:35:15 -0300

    ..

--- LivroJS/paraTestes.js
+++ LivroJS/paraTestes.js
@@ -1,1 +1,17 @@
-let s = 
+
+function arraysIguais(a, b){
+    b = a
+    if(a === b) return true;
+    if(a.length !== b.length) return false;
+
+    for(let i=0; i < a.length; i++){ 
+        if(a[i] !== b[i]) {
+           
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+console.log(arraysIguais([1,2,3,"y"], []));
