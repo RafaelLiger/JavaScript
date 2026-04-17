17f6b922763a837e72e31a7d1b322776dd50471d
Rafael Liger <rafael-liger@outlook.com>
2026-04-17 13:21:23 -0300

    Update prat.js

--- praticar/prat.js
+++ praticar/prat.js
@@ -1,23 +1,5 @@
-function finalizar(){
-      console.log("Tarefas concluidas!")
-  }
-  function fazerTarefa(callback,tempo){
-    setTimeout(callback, Number(tempo))
-    
-  }
-  let acordar = () => {
-    console.log("Acordei!!!")
-  }
-  let dentes = function(){
-    console.log("Escovando dentes...")
-  }
-  function arrumar(){
-    console.log("Colocando roupa...")
-  }
-console.log("Executando tarefas...")   
-fazerTarefa(finalizar, 5000)
-fazerTarefa(arrumar, 4000)
-fazerTarefa(dentes, 3000)
-fazerTarefa(acordar, 2000)
+let arr = [1, 2, 3];
 
+arr[6] = 99;
 
+console.log(arr.length);
