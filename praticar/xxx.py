2f514a2d6699ef42f5c0e0accfa77025a756482f
Rafael Liger <rafael-liger@outlook.com>
2026-04-21 13:14:13 -0300

    ..

--- praticar/xxx.py
+++ praticar/xxx.py
@@ -1,2 +1,12 @@
 nome = input("Digite seu nome: ")
-print("Olá", nome)
+idade = int(input("digite sua idade: "))
+if idade >= 18:
+    classif = "maior de idade" 
+else:
+    classif = "menor de idade"
+print(f"Olá {nome}, você tem {idade} anos e é {classif} ")
+continuar = input("Digite 'exit' para sair?")
+while continuar.lower() != "exit":
+    continuar = input("Comando errado, tente novamente com 'exit': ")
+    
+print("tchau!")   
